f76d0bc65ae4e75a93362c4bc59d86d47fd34d70
Eric_Lian <git@superexercisebook.com>
2020-10-31 22:48:20 +0800

    补充注释

--- Server/Services/Implements/UserService.cs
+++ Server/Services/Implements/UserService.cs
@@ -20,17 +20,28 @@ namespace Server.Services.Implements
             _databaseService = databaseService;
         }
 
+        /// <summary>
+        /// 校验用户密码
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="loginUser"></param>
+        /// <returns></returns>
         public bool IsValid(LoginRequestModel req, out User loginUser)
         {
+            // 从数据库中获取用户信息
             loginUser = _databaseService.Users.Where(s => s.Username == req.Username).FirstOrDefault(null);
 
+
+            // 若该用户不存在
             if (loginUser == null)
             {
                 return false;
             }
 
+            // 检查密码是否正确
             if (BCrypt.Net.BCrypt.Verify(req.Password, loginUser.Password))
             {
+                // 登录成功，更新密码状态并保存
                 loginUser.Password = BCrypt.Net.BCrypt.HashPassword(req.Password);
                 _databaseService.SaveChanges();
             }
