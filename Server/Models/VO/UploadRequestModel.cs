c8d32a69c6919141c6fb9bc6e8655b8edc82942f
Eric_Lian <git@superexercisebook.com>
2020-11-02 11:34:51 +0800

    上传请求

---
+++ Server/Models/VO/UploadRequestModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Server.Models.VO
+{
+    public class UploadRequestModel
+    {
+        [Required]
+        [JsonProperty("Type")]
+        public string Type { get; set; }
+
+
+        [Required]
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [Required]
+        [JsonProperty("size")]
+        public ulong Size { get; set; }
+
+        [Required]
+        [JsonProperty("md5")]
+        public string Md5 { get; set; }
+
+        // TODO 文件元信息
+    }
+}
