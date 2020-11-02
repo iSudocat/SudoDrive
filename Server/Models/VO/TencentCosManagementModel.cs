0eb575e21d5cc40d9844213977c73e2e0e447bfd
Eric_Lian <git@superexercisebook.com>
2020-11-02 09:25:20 +0800

    引入腾讯云 SDK

---
+++ Server/Models/VO/TencentCosManagementModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Server.Models.VO
+{
+    public class TencentCosManagementModel
+    {
+        [JsonProperty("Bucket")]
+        public string Bucket { get; set; }
+
+        [JsonProperty("Region")]
+        public string Region { get; set; }
+
+        [JsonProperty("Prefix")]
+        public string Prefix { get; set; }
+
+        [JsonProperty("SecretId")]
+        public string AllowPrefix { get; set; }
+
+        [JsonProperty("SecretKey")]
+        public string SecretKey { get; set; }
+    }
+}
