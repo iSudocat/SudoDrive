3ce169eaa0e41b0df683910b09d9c36cbde7ee92
A-Salty-Fish <1309032417@qq.com>
2020-11-08 22:00:07 +0800

    侧边栏上传进度条基本绘制完成

--- ClientWebView/src/views/cloudfile/FileState.vue
+++ ClientWebView/src/views/cloudfile/FileState.vue
@@ -1,23 +1,110 @@
 <template>
   <div>
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="" name=""></el-tab-pane>
-      <el-tab-pane label="上传" name="upload">上传</el-tab-pane>
-      <el-tab-pane label="下载" name="download">下载</el-tab-pane>
+    <el-tabs v-model="firstActiveName" @tab-click="handleFirstClick">
+      <el-tab-pane label="" name="" />
+      <el-tab-pane label="上传" name="upload">
+        <el-tabs v-model="uploadActiveName" @tab-click="handleUploadClick">
+          <el-tab-pane label="" name="" />
+          <el-tab-pane label="传输中" name="running">
+            <div
+              v-for="(item, i) in uploads.running"
+              :key="i"
+            >
+              <span>{{ item.name }}</span>
+              <el-progress
+                :text-inside="true"
+                :stroke-width="20"
+                :percentage="item.percentage"
+              />
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="成功" name="success">
+            <div
+              v-for="(item, i) in uploads.success"
+              :key="i"
+            >
+              <span>{{ item.name }}</span>
+              <el-progress
+                :text-inside="true"
+                :stroke-width="20"
+                :percentage="item.percentage"
+                status="success"
+              />
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="失败" name="fail">
+            <div
+              v-for="(item, i) in uploads.fail"
+              :key="i"
+            >
+              <span>{{ item.name }}</span>
+              <el-progress
+                :text-inside="true"
+                :stroke-width="20"
+                :percentage="item.percentage"
+                status="exception"
+              />
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </el-tab-pane>
+      <el-tab-pane label="下载" name="download">
+        <el-tabs v-model="downloadActiveName" @tab-click="handleDownloadClick">
+          <el-tab-pane label="" name="" />
+          <el-tab-pane label="传输中" name="running" />
+          <el-tab-pane label="成功" name="success" />
+          <el-tab-pane label="失败" name="fail" />
+        </el-tabs>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'FileState',
   data() {
     return {
-      activeName: 'download'
+      uploads: {
+        running: [],
+        success: [],
+        fail: []
+      },
+      downloads: {
+        running: [],
+        success: [],
+        fail: []
+      },
+      firstActiveName: 'download',
+      uploadActiveName: 'running',
+      downloadActiveName: 'running'
     }
   },
+  created() {
+    for (let i = 0; i < 5; i++) {
+      this.uploads.running.push({
+        name: '传输中文件' + i,
+        percentage: i * 10
+      })
+      this.uploads.fail.push({
+        name: '失败文件' + i,
+        percentage: 90 - i * 10
+      })
+    }
+    this.uploads.success.push({
+      name: '成功文件',
+      percentage: 100
+    })
+  },
   methods: {
-    handleClick(tab, event) {
+    handleFirstClick(tab, event) {
+      console.log(tab, event)
+    },
+    handleUploadClick(tab, event) {
+      console.log(tab, event)
+    },
+    handleDownloadClick(tab, event) {
       console.log(tab, event)
     }
   }
