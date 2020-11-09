68eed087599f7d3baa3c412b1b227f0606d20f03
rushingfox <jianhaowu2000@gmail.com>
2020-11-09 09:29:36 +0800

    delete the extra codes in addgroupcontroller.cs

--- Server/Controllers/GroupManage/AddGroupController.cs
+++ Server/Controllers/GroupManage/AddGroupController.cs
@@ -39,14 +39,12 @@ namespace Server.Controllers.GroupManage
             _databaseService.Groups.Add(group);
             //obtain the user
             var user = HttpContext.Items["actor"] as User;
-            //find the group,user in the database
-            var group_db = _databaseService.Groups.FirstOrDefault(t=>t.GroupName == group.GroupName);
             var user_db = _databaseService.Users.Find(user.Id);
             
             //initialize grouptouser and save it to database
             GroupToUser groupToUser = new GroupToUser();
-            groupToUser.Group = group_db;
-            groupToUser.GroupId = group_db.Id;
+            groupToUser.Group = group;
+            groupToUser.GroupId = group.Id;
             groupToUser.User = user_db;
             groupToUser.UserId = user_db.Id;
             _databaseService.GroupsToUsersRelation.Add(groupToUser);
@@ -55,12 +53,6 @@ namespace Server.Controllers.GroupManage
             //below is how to input parameters when the entity has composite key values:
             //"The ordering of composite key values is as defined in the EDM, which is in turn as defined in the designer, by the Code First fluent API, or by the DataMember attribute."
             var groupToUser_db = _databaseService.GroupsToUsersRelation.Find(groupToUser.GroupId, groupToUser.UserId);
-
-            group_db.GroupToUser.Add(groupToUser_db);
-            user_db.GroupToUser.Add(groupToUser_db);
-
-            group_db.UpdatedAt = DateTime.Now;
-            user_db.UpdatedAt = group_db.UpdatedAt;
 
             //waiting for adding permissions for the group
             
