f34eca5c5781f479bb872701dc363a8f35bc603c
A-Salty-Fish <1309032417@qq.com>
2020-11-14 19:41:28 +0800

    完成CsJs交互的登录函数

--- Client/CefUtils/Function/CloudFileFunction.cs
+++ Client/CefUtils/Function/CloudFileFunction.cs
@@ -13,9 +13,12 @@ namespace Client.CefUtils.Function
 {
     public class CloudFileFunction
     {
-        public string test()
+        public static UserRequest userService = null;
+        public string login(string userName, string password)
         {
-            return "Test";
+            userService = new UserRequest();
+            userService.Login(userName, password, out LoginResponse loginResponse);
+            return JsonConvert.SerializeObject(loginResponse);
         }
         /// <summary>
         /// 获取云端文件信息
@@ -23,18 +26,23 @@ namespace Client.CefUtils.Function
         /// <returns></returns>
         public string getFileList()
         {
-            UserRequest userService = new UserRequest();
-            userService.Login("sudodog", "ssss11111", out _);
+            if (userService == null) return null;
             FileRequest fileRequest = new FileRequest();
             fileRequest.GetFileList("/users/sudodog/测试数据/a lot of txt",
                 out int status, out List<Client.Request.Response.FileListResponse.File> fileList);
             CloudFileListVO cloudFileListVO = new CloudFileListVO(fileList);
             return JsonConvert.SerializeObject(cloudFileListVO);
         }
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <param name="localPath"></param>
+        /// <param name="cloudPath"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
         public string upload(string localPath, string cloudPath, string fileName)
         {
-            UserRequest userService = new UserRequest();
-            userService.Login("sudodog", "ssss11111", out _);
+            if (userService == null) return null;
             UploadTaskList.Add(new FileControlBlock
             {
                 FileName = fileName,
@@ -44,11 +52,16 @@ namespace Client.CefUtils.Function
             });
             return null;
         }
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="localPath"></param>
+        /// <param name="fileName"></param>
+        /// <param name="guid"></param>
+        /// <returns></returns>
         public string download(string localPath, string fileName, string guid)
         {
-            UserRequest userService = new UserRequest();
-            userService.Login("sudodog", "ssss11111", out _);
-
+            if (userService == null) return null;
             DownloadTaskList.Add(new FileControlBlock
             {
                 FileName = fileName,
