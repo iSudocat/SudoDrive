b35d0d743cbd1ffbb7f85722367406b7a7ee1180
Eric_Lian <git@superexercisebook.com>
2021-05-03 23:24:18 +0800

    fix: 修复列出群组用户时数量错误，修复列出群组用户时用户类型错误

--- Server/Models/VO/GroupMemberListResultModel.cs
+++ Server/Models/VO/GroupMemberListResultModel.cs
@@ -7,15 +7,15 @@ namespace Server.Models.VO
     public class GroupMemberListResultModel
     {
         public Group Group { get; set; }
-        public List<User> Users { get; set; }
+        public List<UserModel> Users { get; set; }
         public int RequestModelAmount { get; set; }
         public int RequestModelOffset { get; set; }
 
         public GroupMemberListResultModel(Group group, List<GroupToUser> users, int requestModelAmount, int requestModelOffset)
         {
             Group = group;
-            Users = users.Select(s => s.User).ToList();
-            RequestModelAmount = requestModelAmount;
+            Users = users.Select(s => new UserModel(s.User)).ToList();
+            RequestModelAmount = users.Count();
             RequestModelOffset = requestModelOffset;
         }
     }
