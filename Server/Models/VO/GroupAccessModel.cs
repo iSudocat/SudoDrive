e668fa1a5c3279d626b85143a659673ee9521c04
Eric_Lian <git@superexercisebook.com>
2021-05-05 21:45:43 +0800

    refactor: 重构 GroupAccessModel

--- Server/Models/VO/GroupAccessModel.cs
+++ Server/Models/VO/GroupAccessModel.cs
@@ -1,38 +1,33 @@
-using System;
 using Server.Models.Entities;
 using Server.Libraries;
 
 namespace Server.Models.VO
 {
-    public class GroupAccessModel
+    public class GroupAccessModel : GroupModel
     {
-        public long Id { get; private init; }
-        public string GroupName { get; private init; }
-        public bool CanAddMember { get; private init;}
-        public bool CanRemoveMember  { get; private init;}
+        public bool CanAddMember { get; private init; }
+        public bool CanRemoveMember { get; private init; }
 
-        public DateTime CreatedAt { get; private init; }
 
-        public DateTime UpdatedAt { get; private init; }
-
-        public GroupAccessModel(Group group, User user)
+        public GroupAccessModel(Group group, User user) : base(group)
         {
-            this.Id = group.Id;
-            this.GroupName = group.GroupName;
-            this.CreatedAt = group.CreatedAt;
-            this.UpdatedAt = group.UpdatedAt;
-
             string addPermission = PermissionBank.GroupOperationPermission(group.GroupName, "member", "add");
-            if (user.HasPermission(addPermission) != true) {
+            if (user.HasPermission(addPermission) != true)
+            {
                 this.CanAddMember = false;
-            } else {
+            }
+            else
+            {
                 this.CanAddMember = true;
             }
-            
+
             string removePermission = PermissionBank.GroupOperationPermission(group.GroupName, "member", "remove");
-            if (user.HasPermission(removePermission) != true) {
+            if (user.HasPermission(removePermission) != true)
+            {
                 this.CanRemoveMember = false;
-            } else {
+            }
+            else
+            {
                 this.CanRemoveMember = true;
             }
         }
